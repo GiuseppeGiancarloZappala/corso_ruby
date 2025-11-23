0e281647202f447962610fae78eb27fca4fc486a
Giuseppe Giancarlo Zappala <giuseppegiancarlozappala@gmail.com>
2025-11-23 14:47:43 +0100

    example puts

---
+++ the_puts_methods.rb
@@ -0,0 +1,18 @@
+puts 1,2,3
+puts "ciao", "ciao", "ciao", "ciao", "ciao"
+puts [1,2,3]
+puts ["ciao", "ciao", "ciao", "ciao", "cia
+o"]
+puts 1
+puts "ciao"
+puts
+puts
+puts nil
+puts false
+puts true
+puts Object.new
+puts :symbol
+puts self
+puts method(:puts)
+puts STDOUT
+puts $stdout
